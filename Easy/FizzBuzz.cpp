df28112444384766cf3c683d8c61f7d262328885
Shailendra Kawadkar <44092499+shailendrakawadkar@users.noreply.github.com>
2022-02-18 18:56:45 -0800

    Create FizzBuzz.cpp

---
+++ Easy/FizzBuzz.cpp
@@ -0,0 +1,23 @@
+vector<string> solve(int n) {
+    vector<string> output;
+    for(int i = 1; i <= n; ++i)
+    {
+        if(i % 3 == 0 && i % 5 == 0)
+        {
+            output.push_back("FizzBuzz");
+        }
+        else if(i % 3 == 0)
+        {
+            output.push_back("Fizz");
+        }
+        else if(i % 5 == 0)
+        {
+            output.push_back("Buzz");
+        }
+        else
+        {
+            output.push_back(to_string(i));
+        }
+    }   
+    return output;
+}
